643c155f4428635f2095bd6805214ded827cb4dc
Atul_Soam <94173614+Atulsoam562003@users.noreply.github.com>
2022-12-21 16:02:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -124,6 +124,7 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function() {
+const PORT = 3000;
+app.listen(process.env.PORT || PORT, function() {
   console.log("Server started on port 3000");
 });
